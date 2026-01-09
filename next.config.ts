71dafeb4cf2d528ae96cc5cd96c412e042ef67e4
Ahmed Ali <a7med3li0111@gmail.com>
2026-01-10 00:13:01 +0200

    handle config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development", // يعمل PWA فقط في الإنتاج
 });
 
 const nextConfig = withPWA({
   reactStrictMode: true,
+  experimental: {
+    turbo: false, // تعطيل Turbopack
+  },
 });
 
 module.exports = nextConfig;

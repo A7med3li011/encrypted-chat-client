893faa3794f3c10383889c5b532bfeccd752eb45
Ahmed Ali <a7med3li0111@gmail.com>
2026-01-20 01:33:16 +0200

    remove proxy

---
+++ proxy.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function proxy(request: NextRequest) {
+  
+}
+
+// Alternatively, you can use a default export:
+// export default function proxy(request: NextRequest) { ... }
+
+
